ed432ca7f78ad0427a577b18948bc1c67a4c868a
Joe Guerra <guerra_joe@hotmail.com>
2018-11-23 19:00:23 -0500

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :workouts
+  resources :workouts do
+  	resources :exercises
+  end
+
+
   root 'workouts#index'
 
 
